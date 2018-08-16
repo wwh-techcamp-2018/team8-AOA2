66abca79f0911be5a1e47565d954e5dceb044b87
woowatechcamp_10 <tech_jhh@woowahan.com>
2018-08-16 22:00:05 +0900

    Add Logback - add logback xml - jpa test & log

--- src/test/java/com/aoa/springwebservice/SpringWebserviceApplicationTests.java
+++ src/test/java/com/aoa/springwebservice/SpringWebserviceApplicationTests.java
@@ -1,18 +1,35 @@
 package com.aoa.springwebservice;
 
+import com.aoa.springwebservice.test.JPATestDomain;
+import com.aoa.springwebservice.test.JPATestDomainRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class SpringWebserviceApplicationTests {
 
+    @Autowired
+    JPATestDomainRepository jpaTestDomainRepository;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void JPAEnvironmentLoad(){
+        JPATestDomain domain = new JPATestDomain();
+        domain.setFlag(true);
+        domain = jpaTestDomainRepository.save(domain);
+        log.debug("domain : {}", domain);
+        assertThat(domain.isFlag()).isEqualTo(jpaTestDomainRepository.findById(domain.getId()).get().isFlag());
     }
 
 }
