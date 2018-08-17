cd03e476fec261c2184abec0383f9bf8620595b7
YeonTaeKIM <urbanscenery@gmail.com>
2018-08-17 16:37:20 +0900

    Add front validator

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
+document.addEventListener('DOMContentLoaded', () => {
+    const counter = $All('.input_counter');
+    M.CharacterCounter.init(counter);
 });
+
+String.prototype.isEmpty = () => {
+    return (this.length === 0 || !this.trim());
+};
+
+const validInput = () => {
+    if($('.invalid')) {
+        $('.invalid').focus();
+        return false;
+    }
+    const emptyValidInputs = Array.from($All('.not-empty'));
+    emptyValidInputs.forEach( e => {
+        if(e.value === ''){
+            e.addClass('invalid');
+            e.focus();
+            return false;
+        }
+    });
+    return true;
+
+};
