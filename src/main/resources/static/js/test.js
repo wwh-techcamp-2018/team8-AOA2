dbebc125bd797eeb9645a15ac08b1bf4c7b932e5
Yun <>
2018-08-18 01:02:04 +0900

    Update Store Register page

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,25 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const counter = $All('.input_counter');
-    M.CharacterCounter.init(counter);
+document.addEventListener('DOMContentLoaded', function () {
+    $('input#input_text, textarea#textarea2').characterCounter();
+});
+document.addEventListener('DOMContentLoaded', function () {
+    $('input#input_text, textarea#textarea2').characterCounter();
 });
 
-String.prototype.isEmpty = () => {
-    return (this.length === 0 || !this.trim());
-};
-
-const validInput = () => {
-    if($('.invalid')) {
+function validated() {
+    if ($('.invalid')) {
         $('.invalid').focus();
         return false;
     }
-    const emptyValidInputs = Array.from($All('.not-empty'));
-    emptyValidInputs.forEach( e => {
-        if(e.value === ''){
-            e.addClass('invalid');
+    const emptyValidInputs = $All('.not-empty'); //Array.from($All('.not-empty'));
+    emptyValidInputs.forEach(e => {
+        if (e.value === '') {
+            e.classList.add('invalid');
             e.focus();
             return false;
         }
     });
     return true;
+}
 
-};
+const fetchAsync = ({url, method, body}) => (
+    fetch(url, {
+        method,
+        body: JSON.stringify(body),
+        headers: {'content-type': 'application/json'},
+        credentials: 'same-origin'
+    })
+        .then(res => {
+            if (!res.ok) {
+                return;
+            }
+            return res.json();
+        })
+);
+
+String.prototype.isEmpty = function () {
+    return (this.length === 0 || !this.trim());
+}
