a1aa7a1093b1be5cfa8f0085d494dbb558b4cdf0
Hong Jun Ho <brenden0730@gmail.com>
2018-08-22 15:17:10 +0900

    Refactor seperate kakao.js file - Footer, LoginFooter 분리 - #todo : 향후 하나로 합치는 작업 필요

--- src/main/java/com/aoa/springwebservice/domain/Store.java
+++ src/main/java/com/aoa/springwebservice/domain/Store.java
@@ -49,6 +49,9 @@ public class Store{
     @Column(nullable = true, length = 600)
     private String description;
 
+    @OneToOne
+    private User user;
+
     // Todo Cascade issue 다른 옵션도 적용해야 할 수도 있음
     @OneToMany(mappedBy = "store", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private List<Menu> menus = new ArrayList<>();
@@ -78,18 +81,19 @@ public class Store{
         return menus.contains(menu);
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         //todo User 추가되면 Store의 unique 제약조건 다시 생각
         Store store = (Store) o;
-        return Objects.equals(storeName, store.storeName);
+        return Objects.equals(storeName, store.storeName) &&
+                Objects.equals(user, store.user);
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(storeName);
+        return Objects.hash(storeName, user);
     }
 }
