a6fbe8f1db8e0550c32d5d474bda5d0dfed5a257
Mi Hyon <mhyun1116@gmail.com>
2018-08-22 10:27:02 +0900

    Add Error Wrapper Class & ValidationErrorResponse
    * Add Error Wrapper Class (ApiError, ValidationError)  and ValidationErrorsResponse
    
    * Add VadliationErrorResponse and handleBindException in DefaultRestControllerAdvice
    
    -not working handleBindException in DefaultRestControllerAdvice
    
    * Fix 주석 설명 추가

--- src/main/java/com/aoa/springwebservice/exception/DefaultRestControllerAdvice.java
+++ src/main/java/com/aoa/springwebservice/exception/DefaultRestControllerAdvice.java
@@ -1,17 +1,26 @@
 package com.aoa.springwebservice.exception;
 
+
+import com.aoa.springwebservice.response.ApiError;
+import com.aoa.springwebservice.response.ValidationErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.BindException;
 
 import javax.persistence.EntityNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
 @RequestMapping("/api")
+@Slf4j
 public class DefaultRestControllerAdvice {
 
     @ExceptionHandler({RuntimeException.class, EntityNotFoundException.class})
@@ -27,6 +36,20 @@ public class DefaultRestControllerAdvice {
         return exception.getMessage();
     }
 
+    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+    protected ResponseEntity<ValidationErrorResponse> handleBindException2( BindException ex, HttpServletRequest request,
+                                                                 HttpServletResponse response, @Nullable Object handler) throws IOException {
+        log.debug("handleBindException 2");
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        log.debug("handler {}",handler.getClass());
+        log.debug("ReseonseEntity {}", ResponseEntity.status(HttpStatus.INSUFFICIENT_STORAGE).body(
+                new ValidationErrorResponse(new ApiError("ERROR MSG", ex.getLocalizedMessage()))
+                        .addAllErrors(ex.getBindingResult())));
+        return ResponseEntity.status(HttpStatus.INSUFFICIENT_STORAGE).body(
+                new ValidationErrorResponse(new ApiError("ERROR MSG", ex.getLocalizedMessage()))
+                        .addAllErrors(ex.getBindingResult())
+        );
+  }
     @ExceptionHandler(UnAuthorizedException.class)
     @ResponseStatus(value = HttpStatus.FORBIDDEN)
     public String unAuthorized(UnAuthorizedException exception) {
