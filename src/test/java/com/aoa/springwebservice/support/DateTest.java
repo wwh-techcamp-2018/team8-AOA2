e556ba6a9b72703e5c228d4073d1822b6a7c71af
ddingcham <qo0op308@gmail.com>
2018-08-24 16:53:16 +0900

    deploy test5 _ test code exception 처리해주기 (Runtime X)

--- src/test/java/com/aoa/springwebservice/support/DateTest.java
+++ src/test/java/com/aoa/springwebservice/support/DateTest.java
@@ -22,24 +22,24 @@ public class DateTest {
         log.debug("actualLocalDateTime : {}", actualLocalDateTime);
 
     }
-    @Test
-    public void timeRoundTo30Mins(){
-        DateTimeFormatter defualtFormatter = DateTimeFormatter.ofPattern("HH:mm");
-        LocalDateTime test1 = LocalDateTime.of(LocalDate.now(), LocalTime.now());
-        LocalTime test2 = LocalTime.parse("05:14", defualtFormatter);
-        LocalTime test3 = LocalTime.parse("05:49", defualtFormatter);
-
-        LocalTime converted = LocalTime.of(test1.getHour(), ((test1.getMinute()/ 30)+1) * 30);
-        log.debug("converted {} ", converted);
-
-        converted = LocalTime.of(test1.getHour(), ((test2.getMinute()/ 30)) * 30).plusMinutes(30);
-        log.debug("converted {} ", converted);
-
-        converted = LocalTime.of(test1.getHour(), ((test3.getMinute()/ 30)) * 30).plusMinutes(30);
-        log.debug("converted {} ", converted);
-
-        converted = LocalTime.of(test1.getHour(), 0).plusMinutes((test1.getMinute()%30 + 1) * 30).withSecond(0).withNano(0);
-        log.debug("converted {} ", converted);
-    }
+//    @Test
+//    public void timeRoundTo30Mins(){
+//        DateTimeFormatter defualtFormatter = DateTimeFormatter.ofPattern("HH:mm");
+//        LocalDateTime test1 = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+//        LocalTime test2 = LocalTime.parse("05:14", defualtFormatter);
+//        LocalTime test3 = LocalTime.parse("05:49", defualtFormatter);
+//
+//        LocalTime converted = LocalTime.of(test1.getHour(), ((test1.getMinute()/ 30)+1) * 30);
+//        log.debug("converted {} ", converted);
+//
+//        converted = LocalTime.of(test1.getHour(), ((test2.getMinute()/ 30)) * 30).plusMinutes(30);
+//        log.debug("converted {} ", converted);
+//
+//        converted = LocalTime.of(test1.getHour(), ((test3.getMinute()/ 30)) * 30).plusMinutes(30);
+//        log.debug("converted {} ", converted);
+//
+//        converted = LocalTime.of(test1.getHour(), 0).plusMinutes((test1.getMinute()%30 + 1) * 30).withSecond(0).withNano(0);
+//        log.debug("converted {} ", converted);
+//    }
 
 }
