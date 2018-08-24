6ff0a87e510b6f0edb0a7eb38cb09f3a46813b7f
YeonTaeKIM <urbanscenery@gmail.com>
2018-08-24 16:03:52 +0900

    Deploy Test

--- src/main/java/com/aoa/springwebservice/web/PageController.java
+++ src/main/java/com/aoa/springwebservice/web/PageController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.EntityExistsException;
 import javax.servlet.http.HttpSession;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
