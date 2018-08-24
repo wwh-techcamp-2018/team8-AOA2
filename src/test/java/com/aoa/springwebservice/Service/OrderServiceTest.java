e98165ac837c852678c23d073b3b5f7d2dcf1fba
Hong Jun Ho <brenden0730@gmail.com>
2018-08-24 13:20:47 +0900

    Refactor Order Test

--- src/test/java/com/aoa/springwebservice/Service/OrderServiceTest.java
+++ src/test/java/com/aoa/springwebservice/Service/OrderServiceTest.java
@@ -31,6 +31,8 @@ public class OrderServiceTest {
     private Store store;
     private Menu menu1;
     private Menu menu2;
+    private Menu menu3;
+    private Menu menu4;
 
     @Autowired
     private UserRepository userRepository;
@@ -63,17 +65,19 @@ public class OrderServiceTest {
 
         menu1 = Menu.builder().store(store).name("test1").description("test1").price(1).id(1L).build();
         menu2 = Menu.builder().store(store).name("test2").description("test2").price(2).id(2L).build();
+        menu3 = Menu.builder().store(store).name("test3").description("test3").price(3).id(3L).build();
+        menu4 = Menu.builder().store(store).name("test4").description("test4").price(4).id(4L).build();
 
         store.addMenu(menu1);
         store.addMenu(menu2);
+        store.addMenu(menu3);
+        store.addMenu(menu4);
         store = storeRepository.save(store);
-
-        orderFormDTO = init_data();
     }
 
     @Test
     public void update_count_Reservations_성공() {
-
+        OrderFormDTO orderFormDTO = createResource(1L, 2L);
         Map<Long, Reservation> result = reservationService.getTodayReservations(store.getId());
 
         //todo : 구매 갯수와 예약 갯수가 일치 (못사는 것에 대한 에러처리)
@@ -87,35 +91,33 @@ public class OrderServiceTest {
         log.debug("newOrder : {}", newOrder);
     }
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void update_count_Reservations_실패() {
 
+        OrderFormDTO orderFormDTO = createResource(3L, 4L);
         orderFormDTO.getOrderItemDTOs().remove(1);
-        orderFormDTO.getOrderItemDTOs().add(new OrderItemDTO(2L, 5));
+        orderFormDTO.getOrderItemDTOs().add(new OrderItemDTO(3L, 5));
 
         Map<Long, Reservation> result = reservationService.getTodayReservations(store.getId());
 
         Order order = orderFormDTO.toDomain(store);
         Order newOrder = orderService.createOrder(result, orderFormDTO, order);
 
-        //todo : runtimeexception 확인
-        assertThat(order.getOrderItems().size()).isEqualTo(0);
-
         log.debug("newOrder : {}", newOrder);
     }
 
-    public OrderFormDTO init_data() {
+    public OrderFormDTO createResource(long menuId1, long menuId2) {
         // When
         List<ReservationDTO> reservationDTOs = Arrays.asList(
                 ReservationDTO.builder()
                         .maxCount(3)
                         .personalMaxCount(3)
-                        .menuId(1L)
+                        .menuId(menuId1)
                         .build(),
                 ReservationDTO.builder()
                         .maxCount(3)
                         .personalMaxCount(3)
-                        .menuId(2L)
+                        .menuId(menuId2)
                         .build());
 
         ReservationFormDTO reservationFormDTO = ReservationFormDTO.builder().hourToClose(12).minuteToClose(0).reservationDTOs(reservationDTOs).build();
@@ -125,8 +127,8 @@ public class OrderServiceTest {
         reservationService.createReservation(reservationFormDTO, store.getId());
 
         List<OrderItemDTO> orderItems = new ArrayList<>();
-        OrderItemDTO orderItemDTO1 = new OrderItemDTO(1L, 1);
-        OrderItemDTO orderItemDTO2 = new OrderItemDTO(2L, 1);
+        OrderItemDTO orderItemDTO1 = new OrderItemDTO(menuId1, 1);
+        OrderItemDTO orderItemDTO2 = new OrderItemDTO(menuId2, 1);
 
         orderItems.add(orderItemDTO1);
         orderItems.add(orderItemDTO2);
