93af92c22246fe3abb963386ddcbf4a93b091a98
Mi Hyon <mhyun1116@gmail.com>
2018-08-30 23:05:26 +0900

    Fix Timepicker & Style (PR #193)
    -Fix Style in displayReservation
    -Fix timepicker in createOrder

--- src/main/java/com/aoa/springwebservice/web/StoreSocketController.java
+++ src/main/java/com/aoa/springwebservice/web/StoreSocketController.java
@@ -2,7 +2,9 @@ package com.aoa.springwebservice.web;
 
 import com.aoa.springwebservice.domain.Order;
 import com.aoa.springwebservice.dto.OrderOutputDTO;
+import com.aoa.springwebservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -15,21 +17,21 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 @Controller
 public class StoreSocket {
+    @Autowired
+    private OrderService orderService;
     @MessageMapping("presentOrders/{storeId}")
     @SendTo("/topic/stores/orders/{storeId}")
-    public OrderOutputDTO updateRealTimeOrder(@DestinationVariable long storeId, @RequestBody OrderOutputDTO order, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
+    public OrderOutputDTO updateRealTimeOrder(@DestinationVariable Long storeId, Long orderId, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
         log.debug("storeId : {}", storeId);
-        log.debug("order message : {}", order);
         //todo DTO 분리 또는 ExtendableDTO 사용
-        return order;
+        return OrderOutputDTO.createOrderOutputDTO(orderService.getOrder(orderId));
     }
 
     @MessageMapping("toastOrders/{storeId}")
     @SendTo("/topic/stores/toasts/{storeId}")
-    public OrderOutputDTO toastOrderInfo(@DestinationVariable long storeId, @RequestBody OrderOutputDTO order, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
+    public OrderOutputDTO toastOrderInfo(@DestinationVariable Long storeId, Long orderId, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
         log.debug("storeId : {}", storeId);
-        log.debug("order message : {}", order);
-        return order;
+        return OrderOutputDTO.createOrderOutputDTO(orderService.getOrder(orderId));
     }
 
 }
