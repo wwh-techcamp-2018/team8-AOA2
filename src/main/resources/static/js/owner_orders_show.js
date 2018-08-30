139954eeb37d67263c04ab2f05a3433708357593
Mi Hyon <mhyun1116@gmail.com>
2018-08-30 13:38:41 +0900

    Refactor showOrder Page Style - CSS

--- src/main/resources/static/js/owner_orders_show.js
+++ src/main/resources/static/js/owner_orders_show.js
@@ -21,57 +21,59 @@ const loadOrders = async (card_content) => {
 
 const timeEventHtmlTmp = ({minHour}, orders)=>  (`<li class="event" data-date="${minHour}"> ${orders} </li>`);
 
-const orderDisplayHtmlTmp = ({ orderItems, name, phoneNumber, pickedup, pickupTime, orderTotalPrice, reservation }) => {
+const orderDisplayHtmlTmp = ({ id, orderItems, name, phoneNumber, pickedup, pickupTime, orderTotalPrice, reservation }) => {
     const orderItemsHtml = orderItems.reduce((accum, cur) => {
         return accum + orderItemsHtmlTmp(cur);
     }, '');
     const offStyle = pickedup ? 'off' : '';
-    return 
-    `<div class="black-wrapper">
-        <span class="card-title activator white-text">수령완료</span>
-        <p >
-            <button class="btn return-btn waves-effect waves-light" type="button" style="min-width: 100px;">
-                되돌리기
-            </button>
-        </p>
-    </div>
-    <div class="card-content ${offStyle}" data-id="${id}" data-pickup-hour="${pickupTime}">
-        <input type="hidden" name="isPickedup" value="${pickedup}" />
-        <span class="card-title activator grey-text text-darken-4">
-            수령인: ${name}
-            <i class="material-icons right">more_vert</i></span>
-        <div class="col s9">
-            <div class="col s12  grey-text">
-                <label>전화번호 : </label>
-                ${phoneNumber}
+    return `
+   <div class="card order-collection">
+       <div class="card-content ${offStyle}" data-id="${id}" data-pickup-hour="${pickupTime}">
+            <div class="black-wrapper">
+                <span class="card-title activator white-text">수령완료</span>
+                <p >
+                    <button class="btn return-btn waves-effect waves-light" type="button" style="min-width: 100px;">
+                        되돌리기
+                               </button>
+                </p>
             </div>
-            <div class="col s12  grey-text">
-                <label>테이크아웃 시간 : </label>
-                ${pickupTime}
-            </div>
-        </div>
-        <p >
-            <button class="btn accept-btn waves-effect waves-light" type="button" style="min-width: 100px;">
-                수령하기
+           <input type="hidden" name="isPickedup" value="${pickedup}" />
+           <span class="card-title activator grey-text text-darken-4">
+               수령인: ${name}
+               <i class="material-icons right">more_vert</i></span>
+           <div class="col s9">
+               <div class="col s12  grey-text">
+                   <label>전화번호 : </label>
+                   ${phoneNumber}
+               </div>
+               <div class="col s12  grey-text">
+                   <label>테이크아웃 시간 : </label>
+                   ${pickupTime}
+               </div>
+           </div>
+           <p >
+               <button class="btn accept-btn waves-effect waves-light" type="button" style="min-width: 100px;">
+                   수령하기
 
-            </button>
-        </p>
-        <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">주문목록<i class="material-icons right">close</i></span>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Menu Name</th>
-                        <th>Item Count</th>
-                        <th>Total Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${orderItemsHtml}
-                </tbody>
-            </table>
-        </div>
-    </div>`;
+           </button>
+           </p>
+           <div class="card-reveal">
+               <span class="card-title grey-text text-darken-4">주문목록<i class="material-icons right">close</i></span>
+               <table>
+                   <thead>
+                       <tr>
+                           <th>Menu Name</th>
+                           <th>Item Count</th>
+                           <th>Total Price</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       ${orderItemsHtml}
+                   </tbody>
+               </table>
+           </div>
+       </div>
+   </div>`;
 };
 
 
