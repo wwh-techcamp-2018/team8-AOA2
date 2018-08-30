beef752194619e5660ede56345f48c09303fc402
Mi Hyon <mhyun1116@gmail.com>
2018-08-30 23:12:06 +0900

    Update StoreSocketController.java

--- src/main/java/com/aoa/springwebservice/web/StoreSocketController.java
+++ src/main/java/com/aoa/springwebservice/web/StoreSocketController.java
@@ -16,22 +16,22 @@ import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
-public class StoreSocket {
-    @Autowired
-    private OrderService orderService;
+public class StoreSocketController {
     @MessageMapping("presentOrders/{storeId}")
     @SendTo("/topic/stores/orders/{storeId}")
-    public OrderOutputDTO updateRealTimeOrder(@DestinationVariable Long storeId, Long orderId, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
+    public OrderOutputDTO updateRealTimeOrder(@DestinationVariable long storeId, @RequestBody OrderOutputDTO order, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
         log.debug("storeId : {}", storeId);
+        log.debug("order message : {}", order);
         //todo DTO 분리 또는 ExtendableDTO 사용
-        return OrderOutputDTO.createOrderOutputDTO(orderService.getOrder(orderId));
+        return order;
     }
 
     @MessageMapping("toastOrders/{storeId}")
     @SendTo("/topic/stores/toasts/{storeId}")
-    public OrderOutputDTO toastOrderInfo(@DestinationVariable Long storeId, Long orderId, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
+    public OrderOutputDTO toastOrderInfo(@DestinationVariable long storeId, @RequestBody OrderOutputDTO order, SimpMessageHeaderAccessor simpMessageHeaderAccessor) {
         log.debug("storeId : {}", storeId);
-        return OrderOutputDTO.createOrderOutputDTO(orderService.getOrder(orderId));
+        log.debug("order message : {}", order);
+        return order;
     }
 
 }
